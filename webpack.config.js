5a3a1edc56c813a76ec494b0c491384b526cb1f4
Ashwin Naik <ashwinteritree@gmail.com>
2017-05-11 03:16:46 +0530

    Build configuration update to support react - Updating build system with all the dependencies and libraries - Providing for build with npm build and npm run commands - Making a mock version of broadcast without authentication

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './client/app.js',
+    plugins: [
+        new webpack.ProvidePlugin({
+           $: "jquery",
+           jQuery: "jquery"
+       })
+   ],
+    output:{
+        path: __dirname + '/dist',
+        publicPath:'/dist/',
+        filename:'bundle.js'
+    },
+    module:{
+
+        loaders:[
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query:{
+                    presets:['react','es2015']
+                }
+            },
+            {
+                test:/\.css/,
+                loader: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
+
+        ]
+    }
+}
